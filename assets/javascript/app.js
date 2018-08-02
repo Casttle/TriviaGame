d23d4c72bc8c55e56e1903916adc945829406177
Casttle <cberry1222@gmail.com>
2018-08-02 15:20:00 -0500

    initial files

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+// Starting with the basic quiz
+//When the page loads the player will see the Title, countdown timer, and questions
+// all of the questions will have multiple choice options for answers.
+//each answer block will trake if it is right, wrong, or unanswered
+//When the timer reaches 0:00 the game ends and the page will be replaced to
+//show right, wrong, unanswered
+//Hitting reset will restart the quiz not reload the page.
